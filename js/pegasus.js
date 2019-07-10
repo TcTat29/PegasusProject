2e0225c32720d76312c88a5f470b55b3b081cc95
TcTat29 <mkphm8@163.com>
2019-07-10 16:32:57 +0800

    2019/07/10

---
+++ js/pegasus.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".pgs-toggle-sidebar").click(function () {
+        if ($(".pgs-sidebar").hasClass("pgs-sidebar-complete")) {
+            $(".pgs-sidebar").removeClass("pgs-sidebar-complete");
+            $(".pgs-sidebar").addClass("pgs-sidebar-lite");
+            $(".pgs-layout").removeClass("pgs-layout-with-complete-sidebar");
+            $(".pgs-layout").addClass("pgs-layout-with-lite-sidebar");
+        } else {
+            $(".pgs-sidebar").removeClass("pgs-sidebar-lite");
+            $(".pgs-sidebar").addClass("pgs-sidebar-complete");
+            $(".pgs-layout").removeClass("pgs-layout-with-lite-sidebar");
+            $(".pgs-layout").addClass("pgs-layout-with-complete-sidebar");
+        }
+    })
+})
