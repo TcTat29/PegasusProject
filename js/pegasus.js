48c9cb00e4e6c224848620b62109532facc9f60f
TheComingThrough@29 <mkphm8@163.com>
2019-07-13 17:03:51 +0800

    2019/07/13

--- js/pegasus.js
+++ js/pegasus.js
@@ -5,11 +5,15 @@ $(function () {
             $(".pgs-sidebar").addClass("pgs-sidebar-lite");
             $(".pgs-layout").removeClass("pgs-layout-with-complete-sidebar");
             $(".pgs-layout").addClass("pgs-layout-with-lite-sidebar");
+            $(".pgs-footer").removeClass("pgs-footer-with-complete-sidebar");
+            $(".pgs-footer").addClass("pgs-footer-with-lite-sidebar");
         } else {
             $(".pgs-sidebar").removeClass("pgs-sidebar-lite");
             $(".pgs-sidebar").addClass("pgs-sidebar-complete");
             $(".pgs-layout").removeClass("pgs-layout-with-lite-sidebar");
             $(".pgs-layout").addClass("pgs-layout-with-complete-sidebar");
+            $(".pgs-footer").removeClass("pgs-footer-with-lite-sidebar");
+            $(".pgs-footer").addClass("pgs-footer-with-complete-sidebar");
         }
     })
 })
