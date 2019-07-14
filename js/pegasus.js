7af296c3946ca590de17bd19669691fd80d1c064
TheComingThrough@29 <mkphm8@163.com>
2019-07-14 17:02:29 +0800

    2019/07/14

--- js/pegasus.js
+++ js/pegasus.js
@@ -3,17 +3,27 @@ $(function () {
         if ($(".pgs-sidebar").hasClass("pgs-sidebar-complete")) {
             $(".pgs-sidebar").removeClass("pgs-sidebar-complete");
             $(".pgs-sidebar").addClass("pgs-sidebar-lite");
+            $(".pgs-header").removeClass("pgs-layout-with-complete-sidebar");
+            $(".pgs-header").addClass("pgs-layout-with-lite-sidebar");
             $(".pgs-layout").removeClass("pgs-layout-with-complete-sidebar");
             $(".pgs-layout").addClass("pgs-layout-with-lite-sidebar");
-            $(".pgs-footer").removeClass("pgs-footer-with-complete-sidebar");
-            $(".pgs-footer").addClass("pgs-footer-with-lite-sidebar");
+            $(".pgs-footer").removeClass("pgs-layout-with-complete-sidebar");
+            $(".pgs-footer").addClass("pgs-layout-with-lite-sidebar");
+            $(".pgs-logo-title").removeClass("pgs-logo-title-with-complete-sidebar");
+            $(".pgs-logo-title").addClass("pgs-logo-title-with-lite-sidebar");
+            // $(".pgs-toggle-sidebar").find("i").addClass("fa-rotate-180");
         } else {
             $(".pgs-sidebar").removeClass("pgs-sidebar-lite");
             $(".pgs-sidebar").addClass("pgs-sidebar-complete");
+            $(".pgs-header").removeClass("pgs-layout-with-lite-sidebar");
+            $(".pgs-header").addClass("pgs-layout-with-complete-sidebar");
             $(".pgs-layout").removeClass("pgs-layout-with-lite-sidebar");
             $(".pgs-layout").addClass("pgs-layout-with-complete-sidebar");
-            $(".pgs-footer").removeClass("pgs-footer-with-lite-sidebar");
-            $(".pgs-footer").addClass("pgs-footer-with-complete-sidebar");
+            $(".pgs-footer").removeClass("pgs-layout-with-lite-sidebar");
+            $(".pgs-footer").addClass("pgs-layout-with-complete-sidebar");
+            $(".pgs-logo-title").removeClass("pgs-logo-title-with-lite-sidebar");
+            $(".pgs-logo-title").addClass("pgs-logo-title-with-complete-sidebar");
+            // $(".pgs-toggle-sidebar").find("i").removeClass("fa-rotate-180");
         }
     })
 })
